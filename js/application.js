d00ded6eafb21b9965e28c306dfdaae9fa2d3dac
fremiller <10870360+fremiller@users.noreply.github.com>
2022-05-10 09:28:52 +0000

    Update gh-pages to output generated at ad2be0d

--- js/application.js
+++ js/application.js
@@ -7,32 +7,32 @@
     new GameManager( 4, KeyboardInputManager, HTMLActuator, LocalStorageManager );
   } );
 
-  addToHomescreen( {
-    autostart: true,
-    autoHide: 0,
-    logging: true,
-    minSessions: 2,
-    onShow: function () {
-      console.log( "showing" );
-    },
-    onInit: function () {
-      console.log( "initializing" );
-    },
-    onAdd: function () {
-      console.log( "adding" );
-    },
-    onInstall: function () {
-      console.log( "Installing" );
-    },
-    onCancel: function () {
-      console.log( "Cancelling" );
-    },
-    displayNextPrime: true,
-    customCriteria: true,
-    displayPace: 0,
-    customPrompt: {
-      src: "meta/icon-48x48.png"
-    }
-  } );
+  // addToHomescreen( {
+  //   autostart: true,
+  //   autoHide: 0,
+  //   logging: true,
+  //   minSessions: 2,
+  //   onShow: function () {
+  //     console.log( "showing" );
+  //   },
+  //   onInit: function () {
+  //     console.log( "initializing" );
+  //   },
+  //   onAdd: function () {
+  //     console.log( "adding" );
+  //   },
+  //   onInstall: function () {
+  //     console.log( "Installing" );
+  //   },
+  //   onCancel: function () {
+  //     console.log( "Cancelling" );
+  //   },
+  //   displayNextPrime: true,
+  //   customCriteria: true,
+  //   displayPace: 0,
+  //   customPrompt: {
+  //     src: "meta/icon-48x48.png"
+  //   }
+  // } );
 
 } )();
